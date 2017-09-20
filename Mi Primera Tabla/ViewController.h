d0838952eee123cf7e05f2f20ebdeaf4bb9f220a
icarmonag <31824460+icarmonag@users.noreply.github.com>
2017-09-20 18:33:43 -0500

    Initial Commit

---
+++ Mi Primera Tabla/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mi Primera Tabla
+//
+//  Created by Ivan on 9/20/17.
+//  Copyright © 2017 wondererApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mi Primera Tabla/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mi Primera Tabla
+//
+//  Created by Ivan on 9/20/17.
+//  Copyright © 2017 wondererApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
